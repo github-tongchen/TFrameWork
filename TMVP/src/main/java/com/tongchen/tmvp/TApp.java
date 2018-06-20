89928a6cdee05c7b25764eee7fec46355d0815fb
tongchen <839706407@qq.com>
2018-06-20 13:14:16 +0800

    添加依赖

---
+++ TMVP/src/main/java/com/tongchen/tmvp/TApp.java
@@ -0,0 +1,11 @@
+package com.tongchen.tmvp;
+
+import android.app.Application;
+
+/**
+ * Created by TongChen at 12:43 on 2018/6/20.
+ * <p>
+ * Description:该文件实现的功能
+ */
+public class TApp extends Application {
+}
