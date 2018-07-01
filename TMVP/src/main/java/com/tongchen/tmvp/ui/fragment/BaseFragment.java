ceaf4d7bb243b76c5fe4e90477235c16a9492089
tongchen <839706407@qq.com>
2018-07-01 22:42:00 +0800

    修改部分MVP架构

--- TMVP/src/main/java/com/tongchen/tmvp/ui/fragment/BaseFragment.java
+++ TMVP/src/main/java/com/tongchen/tmvp/ui/fragment/BaseFragment.java
@@ -2,6 +2,7 @@ package com.tongchen.tmvp.ui.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,8 +19,9 @@ public class BaseFragment extends Fragment {
 
     }
 
+    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.tmvp_fragment_base, container, false);
     }
 }
--- TMVP/src/main/java/com/tongchen/tmvp/ui/fragment/MVPFragment.java
+++ TMVP/src/main/java/com/tongchen/tmvp/ui/fragment/MVPFragment.java
@@ -2,6 +2,7 @@ package com.tongchen.tmvp.ui.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,11 +16,12 @@ public class MVPFragment extends BaseFragment {
 
     public MVPFragment() {
 
-        Class a=MVPFragment.class;
     }
 
+    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.tmvp_fragment_base, container, false);
     }
+
 }
