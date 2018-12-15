cad870233f529a2b7a13f8a46e84169ca96d5310
TongChen <839706407@qq.com>
2018-12-15 19:19:23 +0800

    使用一个委婉的方法：给AppBarLayout设置一个跟状态栏高度一致的paddingTop，来使得gank_fragment能够沉浸式而不影响gank_fragment_content_text的沉浸式。

--- app_watcher/src/main/java/com/tongchen/twatcher/gank/ui/fragment/ContentTextFragment.java
+++ app_watcher/src/main/java/com/tongchen/twatcher/gank/ui/fragment/ContentTextFragment.java
@@ -1,15 +1,26 @@
 package com.tongchen.twatcher.gank.ui.fragment;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.tongchen.twatcher.R;
 import com.tongchen.twatcher.base.ui.fragment.BaseFragment;
 import com.tongchen.twatcher.gank.model.entity.GankResult;
 import com.tongchen.twatcher.util.LogUtils;
+
+import java.util.List;
 
 import butterknife.BindView;
 
@@ -23,8 +34,14 @@ public class ContentTextFragment extends BaseFragment {
 
     @BindView(R.id.iv_head_bg)
     ImageView mHeadBgIv;
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.progressbar_4_web)
+    ProgressBar mWebProgressBar;
     @BindView(R.id.web_content)
     WebView mWebView;
+    @BindView(R.id.tv_publish_date)
+    TextView mPublishDateTv;
 
     public ContentTextFragment() {
     }
@@ -55,8 +72,48 @@ public class ContentTextFragment extends BaseFragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        Glide.with(mActivity).load(mGankResult.getImages().get(0)).into(mHeadBgIv);
+        List<String> imgList = mGankResult.getImages();
+        if (imgList != null && imgList.size() > 0) {
+            Glide.with(mActivity).load(imgList.get(0)).into(mHeadBgIv);
+        }
+        mToolbar.setTitle(mGankResult.getDesc());
+        mPublishDateTv.setText(mGankResult.getPublishedAt().split("T")[0]);
+        //  先隐藏加载完成后再显示
+        mPublishDateTv.setVisibility(View.GONE);
+
+        mWebView.setWebViewClient(new WebViewClient() {
+
+            @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                mWebView.loadUrl(request.getUrl().toString());
+                return true;
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                mWebView.loadUrl(url);
+                return true;
+            }
+        });
+
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                //  进度条处理
+                if (mWebProgressBar.getVisibility() == View.GONE) {
+                    mWebProgressBar.setVisibility(View.VISIBLE);
+                }
+                mWebProgressBar.setProgress(newProgress);
+                if (newProgress == 100) {
+                    mWebProgressBar.setVisibility(View.GONE);
+                    mPublishDateTv.setVisibility(View.VISIBLE);
+                }
+            }
+        });
         mWebView.loadUrl(mGankResult.getUrl());
+
     }
 
     public void updateData(GankResult result) {
