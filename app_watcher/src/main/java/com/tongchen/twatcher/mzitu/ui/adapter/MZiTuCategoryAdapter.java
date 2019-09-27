e906bbf8d6733adb2d09bf2742b6d9f1edbb563a
tongchen <839706407@qq.com>
2019-09-27 20:43:57 +0800

    去除Header 里的host,解决因妹子图官网改动导致的图片无法展示的问题。

--- app_watcher/src/main/java/com/tongchen/twatcher/mzitu/ui/adapter/MZiTuCategoryAdapter.java
+++ app_watcher/src/main/java/com/tongchen/twatcher/mzitu/ui/adapter/MZiTuCategoryAdapter.java
@@ -51,7 +51,7 @@ public class MZiTuCategoryAdapter extends BaseQuickAdapter<MZiTu, BaseViewHolder
         } else {
             return new GlideUrl(url, new LazyHeaders.Builder()
                     .addHeader("Accept-Language", "zh-CN,zh;q=0.9,zh-TW;q=0.8")
-                    .addHeader("Host", "i.meizitu.net")
+//                    .addHeader("Host", "i.meizitu.net")
                     .addHeader("Referer", "http://www.mzitu.com/")
                     .addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36")
                     .build());
